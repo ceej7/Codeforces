e786b26dd1f2c256370e3975e8479ba888205626
ceej7 <ceej_7@hotmail.com>
2020-11-18 12:16:27 +0800

    build

--- BuildAndRun.bat
+++ BuildAndRun.bat
@@ -1,1 +1,9 @@
-cmake -G "Visual Studio 16 2019" -S . -B "proj"
+@echo off
+SET CMAKE_GENERATOR="Unknown Generator"
+reg query "HKEY_CLASSES_ROOT\VisualStudio.DTE.16.0" >> nul 2>&1
+if %ERRORLEVEL% NEQ 0 (
+	SET CMAKE_GENERATOR="Visual Studio 15 2017"
+) else (
+	SET CMAKE_GENERATOR="Visual Studio 16 2019"
+)
+cmake -G %CMAKE_GENERATOR% -S . -B "proj"
