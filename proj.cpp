47766a6b2c641ad812c6a8c0d883f3c4229e1f4c
ceej7 <ceej_7@hotmail.com>
2021-01-25 10:02:02 +0800

    misc

--- proj.cpp
+++ proj.cpp
@@ -9,6 +9,9 @@
 #include <set>
 #include <d3d12.h>
 #include <wrl.h>
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
 #include <guiddef.h>
 using namespace std;
 
@@ -103,16 +106,25 @@ struct Allocation {
 	}
 };
 
+UINT32 PackDrawMletPrimId(UINT32 drawId, UINT32 use32Index, UINT32 meshletId, UINT32 primitiveId)
+{
+	UINT32 drawId_mletId_primId = ((drawId << 25) & 0xFE000000) | ((use32Index << 24) & 0x01000000) | ((meshletId << 5) & 0x00FFFFE0) | ((primitiveId << 0) & 0x0000001F);
+	return drawId_mletId_primId;
+}
+
 int main(int argc, char* argv[]) {
 
-	std::vector<int> vec(4);
-	vec[0] = 99; vec[1] = 99;
-	vec[2] = 97;
-	vec[3] = 95;
-	vec.resize(1);
-	vec.resize(10);
+	//std::vector<int> vec(4);
+	//vec[0] = 99; vec[1] = 99;
+	//vec[2] = 97;
+	//vec[3] = 95;
+	//vec.resize(1);
+	//vec.resize(10);
 
-	std::cout<<"----------------------------------" << std::endl;
+
+
+
+	//std::cout<<"----------------------------------" << std::endl;
 	//std::set<Allocation> allocations;
 
 	//allocations.emplace(Allocation(0, 16));
@@ -124,4 +136,70 @@ int main(int argc, char* argv[]) {
 	//printf("------------------------------\n");
 	//auto it = allocations.find(Allocation(16));
 	//printf("%d, %d\n", it->offset, it->size);
+	
+//#define PARTICLE_OIT_MAX_DEPTH 8
+//	int layerCount = 0;
+//	int layers[PARTICLE_OIT_MAX_DEPTH] = { 0,1,7,2,4,5,6,3 };
+//	std::srand(std::time(nullptr));
+//	for (int i = 0; i < PARTICLE_OIT_MAX_DEPTH; i++) {
+//		layers[i]=std::rand() / ((RAND_MAX + 1u) / 36);
+//	}
+//	for (; layerCount < PARTICLE_OIT_MAX_DEPTH; layerCount++) {
+//		int tmpLayer = layers[layerCount];
+//		//insertion sort
+//		int j = layerCount - 1;
+//		for (; j >= 0&&layers[j] > tmpLayer; j--) {
+//			layers[j + 1] = layers[j];
+//		}
+//		layers[j + 1] = tmpLayer;
+//	}
+//	printf("aa\n");
+
+	//UINT32 a = 98;
+	//UINT32 b = 0;
+	//UINT32 c = 454324;
+	//UINT32 d = 27;
+	//UINT32 packedId = PackDrawMletPrimId(a, b, c, d);
+	//UINT32 a1 = (packedId & 0xFE000000) >> 25;
+	//UINT32 b1 = (packedId & 0x01000000) >> 24;
+	//UINT32 c1 = (packedId & 0x00FFFFE0) >> 5;
+	//UINT32 d1 = (packedId & 0x0000001F) >> 0;
+	//printf("%d-%d-%d-%d\n", a1, b1, c1, d1);
+
+	void* a[2][5] = {
+		{reinterpret_cast<void*>(1),reinterpret_cast<void*>(1),reinterpret_cast<void*>(1),reinterpret_cast<void*>(1),reinterpret_cast<void*>(1)},
+		{reinterpret_cast<void*>(1),reinterpret_cast<void*>(1),reinterpret_cast<void*>(1),reinterpret_cast<void*>(1),reinterpret_cast<void*>(1)}
+	};
+
+	int b = sizeof(a);
+	int c = sizeof(a) / sizeof(a[0][0]);
+
+	memset(a, 0, b - 1* sizeof(a[0][0]));
+
+
+	//std::vector<UINT> m_vec;
+	//m_vec.reserve(10);
+	//std::cout << m_vec.capacity() << std::endl;
+	//m_vec.push_back(1);
+	//m_vec.push_back(1);
+	//m_vec.push_back(1);
+	//std::cout << m_vec.capacity() << std::endl;
+	//m_vec.clear();
+	//std::cout << m_vec.capacity() << std::endl;
+
+	struct SMiscData {
+		UINT g_LoadVertexInfo[4];
+		UINT g_BatchOffset;
+		UINT g_PackedSamplerId;
+		UINT padding[2];
+	};
+
+	std::cout << sizeof(SMiscData) << std::endl;
+
+
+	system("pause");
+	//uint4 UnpackDrawMletPrimId(uint packedId) {
+	//	return uint4((packedId & 0xFE000000) >> 25, (packedId & 0x01000000) >> 24, (packedId & 0x00FFFFE0) >> 5, (packedId & 0x0000001F) >> 0);
+	//}
+
 }
